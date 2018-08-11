97baeebca8307174a9a6e9e5c585f0b3523c1d5a
pagkly <ajsgisayid@gmail.com>
2018-08-11 16:48:48 +1000

    commit by w10

--- settingup.bat
+++ settingup.bat
@@ -1,8 +1,64 @@
 REM C:\Users\SP3\AppData\Roaming\FiiNote
+@echo off
+setlocal enabledelayedexpansion enableextensions
+
+ CLS
+ ECHO.
+ ECHO =============================
+ ECHO Running Admin shell
+ ECHO =============================
+
+:init
+ setlocal DisableDelayedExpansion
+ set cmdInvoke=1
+ set winSysFolder=System32
+ set "batchPath=%~0"
+ for %%k in (%0) do set batchName=%%~nk
+ set "vbsGetPrivileges=%temp%\OEgetPriv_%batchName%.vbs"
+ setlocal EnableDelayedExpansion
+
+:checkPrivileges
+  NET FILE 1>NUL 2>NUL
+  if '%errorlevel%' == '0' ( goto gotPrivileges ) else ( goto getPrivileges )
+
+:getPrivileges
+  if '%1'=='ELEV' (echo ELEV & shift /1 & goto gotPrivileges)
+  ECHO.
+  ECHO **************************************
+  ECHO Invoking UAC for Privilege Escalation
+  ECHO **************************************
+
+  ECHO Set UAC = CreateObject^("Shell.Application"^) > "%vbsGetPrivileges%"
+  ECHO args = "ELEV " >> "%vbsGetPrivileges%"
+  ECHO For Each strArg in WScript.Arguments >> "%vbsGetPrivileges%"
+  ECHO args = args ^& strArg ^& " "  >> "%vbsGetPrivileges%"
+  ECHO Next >> "%vbsGetPrivileges%"
+
+  if '%cmdInvoke%'=='1' goto InvokeCmd 
+
+  ECHO UAC.ShellExecute "!batchPath!", args, "", "runas", 1 >> "%vbsGetPrivileges%"
+  goto ExecElevation
+
+:InvokeCmd
+  ECHO args = "/c """ + "!batchPath!" + """ " + args >> "%vbsGetPrivileges%"
+  ECHO UAC.ShellExecute "%SystemRoot%\%winSysFolder%\cmd.exe", args, "", "runas", 1 >> "%vbsGetPrivileges%"
+
+:ExecElevation
+ "%SystemRoot%\%winSysFolder%\WScript.exe" "%vbsGetPrivileges%" %*
+ exit /B
+
+:gotPrivileges
+ setlocal & cd /d %~dp0
+ if '%1'=='ELEV' (del "%vbsGetPrivileges%" 1>nul 2>nul  &  shift /1)
+ 
 set "fnnotesdir=%APPDATA%\FiiNote\@pagkly"
 set "githubdir="%USERPROFILE%\Documents\Github\FNnotes\@pagkly"
 if exist %fnnotesdir% (
-move %fnnotesdir% %fnnotesdir%old
+REM move %fnnotesdir% %fnnotesdir%old
+robocopy %fnnotesdir% %fnnotesdir%old /E /IS /MOVE
+REM mkdir %fnnotesdir%old
+REM move /y %fnnotesdir% %fnnotesdir%old
+rmdir /S %fnnotesdir%
 )
 REM mkdir %fnnotesdir%
-mklink /d %fnnotesdir% %githubdir%
+mklink /D %fnnotesdir% %githubdir%
